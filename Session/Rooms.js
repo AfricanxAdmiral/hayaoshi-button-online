53b95a799662dbc74720019af40397218cd59391
AficanxAdmiral <guiltyxapple@gmail.com>
2023-11-14 16:14:23 +0800

    remove sample and uuid

--- Session/Rooms.js
+++ Session/Rooms.js
@@ -1,5 +1,5 @@
 const Session = require('./Session');
-const uuid = require('uuid');
+// const uuid = require('uuid');
 
 module.exports = class Rooms {
     constructor(io) {
@@ -8,7 +8,8 @@ module.exports = class Rooms {
     }
 
     createNewRoom(options = {}) {
-        const id = uuid.v4();
+        // const id = uuid.v4();
+        const id = '0af9eedf-0410-4ef7-8a12-e47a880d7463';
         this.rooms.push(new Session(this.io.of(`/session/${id}`), options));
         return id;
     }
